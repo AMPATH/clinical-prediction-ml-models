a5421d03288e8f77e934d8d7fcca201ba49ef983
Ian <ian_bacher@brown.edu>
2023-06-20 11:20:20 -0400

    Add latest version of production SQL

--- SQL/iit_prod_data_extract.sql
+++ SQL/iit_prod_data_extract.sql
@@ -1,85 +1,53 @@
--- Our ML Query
--- First, we filter the flat_hiv_summary_v15b table to give us only rows we care about
-with ml_flat_hiv_summary as (
+-- ML query
+-- Calculate rolling default numbers
+with num_2wk_defaults_last_3_visits as (
     select
-        fs.*
-    from flat_hiv_summary_v15b fs
-    where arv_start_date is not null
-      and is_clinical_encounter = 1
-      and encounter_datetime >= date('2018-01-01')
-      and not (
-            rtc_date = prev_rtc_date
-        and encounter_type = prev_encounter_type_hiv
-        and encounter_datetime = prev_encounter_datetime_hiv
-    )
+        dd1.person_id,
+        dd1.encounter_id,
+        dd1.visit_number,
+        if(dd2.days_defaulted_last_encounter >= 14, 1, 0) +
+        if(dd3.days_defaulted_last_encounter >= 14, 1, 0) +
+        if(dd4.days_defaulted_last_encounter >= 14, 1, 0) as num_2wks_defaults_last_3visits
+    from predictions.flat_ml_days_defaulted dd1
+             left join predictions.flat_ml_days_defaulted dd2
+                       on dd2.person_id = dd1.person_id
+                           and dd2.encounter_id = dd1.encounter_id
+                           and dd2.visit_number = dd1.visit_number - 1
+             left join predictions.flat_ml_days_defaulted dd3
+                       on dd3.person_id = dd2.person_id
+                           and dd3.encounter_id = dd2.encounter_id
+                           and dd3.visit_number = dd2.visit_number - 1
+             left join predictions.flat_ml_days_defaulted dd4
+                       on dd4.person_id = dd3.person_id
+                           and dd4.encounter_id = dd3.encounter_id
+                           and dd4.visit_number = dd3.visit_number - 1
 ),
--- Then, we calculate visit_numbers (really encounter numbers) as number of visits since the start date
-visit_numbers as (
+defaults_by_days as (
     select
-        row_number() over (partition by person_id order by encounter_datetime) as visit_number,
-        person_id,
-        encounter_id
-    from ml_flat_hiv_summary
-),
--- Combine these into one table
-ml_flat_hiv_summary_with_encounter_number as (
-    select
-        fs.*,
-        visit_number
-    from ml_flat_hiv_summary fs
-        join visit_numbers vn
-          on fs.person_id = vn.person_id
-         and fs.encounter_id = vn.encounter_id
-),
--- Then, we check for the days defaulted in the previous encounter
-days_defaulted as (
-    select
-        person_id,
-        encounter_id,
-        encounter_datetime,
-        prev_clinical_rtc_date_hiv,
-        datediff(encounter_datetime, prev_clinical_rtc_date_hiv) as days_defaulted
-    from ml_flat_hiv_summary_with_encounter_number
-),
--- Calculate some rolling aggregates (joins should ensure we are not over-calculating)
-num_2wk_defaults_last_3_visits as (
-    select
-            fs.person_id,
-            fs.encounter_id,
-            if(dd1.days_defaulted >= 14, 1, 0) +
-            if(dd2.days_defaulted >= 14, 1, 0) +
-            if(dd3.days_defaulted >= 14, 1, 0) as num_2wks_defaults_last_3visits
-    from ml_flat_hiv_summary_with_encounter_number fs
-      left join ml_flat_hiv_summary_with_encounter_number fs_visit_1
-         on fs_visit_1.person_id = fs.person_id and fs_visit_1.visit_number = fs.visit_number - 1
-      left join days_defaulted dd1
-         on dd1.person_id = fs_visit_1.person_id
-        and dd1.encounter_id = fs_visit_1.encounter_id
-      left join ml_flat_hiv_summary_with_encounter_number fs_visit_2
-         on fs_visit_2.person_id = fs_visit_1.person_id and fs_visit_2.visit_number = fs_visit_1.visit_number - 1
-      left join days_defaulted dd2
-         on dd2.person_id = fs_visit_2.person_id
-        and dd2.encounter_id = fs_visit_2.encounter_id
-      left join ml_flat_hiv_summary_with_encounter_number fs_visit_3
-         on fs_visit_3.person_id = fs_visit_2.person_id and fs_visit_3.visit_number = fs_visit_2.visit_number - 1
-      left join days_defaulted dd3
-         on dd3.person_id = fs_visit_3.person_id
-        and dd3.encounter_id = fs_visit_3.encounter_id
-)
+        dd.person_id,
+        dd.encounter_id,
+        encounter_date,
+        max(dd.days_defaulted_last_encounter) as days_defaulted
+    from predictions.flat_ml_days_defaulted dd
+    group by dd.person_id, encounter_date
+ )
 select
     fs.person_id,
     fs.encounter_id,
+    fs.encounter_type,
+    fs.location_id,
+    fs.rtc_date,
     timestampdiff(YEAR, p.birthdate, fs.encounter_datetime) as Age,
     if(p.birthdate is null, 1, 0) as Age_NA,
     p.gender as Gender,
     null as Marital_status,
     timestampdiff(YEAR,
-        if(year(fs.arv_first_regimen_start_date) != 1900,
-            date(fs.arv_first_regimen_start_date),
-            NULL
-        ), DATE(fs.encounter_datetime)) as Duration_in_HIV_care,
+                  if(year(fs.arv_first_regimen_start_date) != 1900,
+                     date(fs.arv_first_regimen_start_date),
+                     NULL
+                      ), date(fs.encounter_datetime)) as Duration_in_HIV_care,
     if(fs.arv_first_regimen_start_date is null or year(fs.arv_first_regimen_start_date) = 1900,
-        1, 0) as Duration_in_HIV_care_NA,
+       1, 0) as Duration_in_HIV_care_NA,
     round(fs.weight / ((fs.height / 100) * (fs.height / 100)), 2) as BMI,
     if(fs.weight is null or fs.height is null or fs.weight < 1 or fs.height < 1, 1, 0) as BMI_NA,
     null as Travel_time,
@@ -100,68 +68,90 @@ select
             then 'Urban'
         when fs.location_id in (65, 314, 64, 83, 316, 90, 135, 106, 86, 336, 91, 320, 74, 76, 79, 100, 311, 75)
             then 'Rural'
-    end as Clinic_Location,
+        end as Clinic_Location,
     null as TB_Comorbidity,
     fs.cd4_resulted as CD4,
     if(fs.cd4_resulted is null, 1, 0) as CD4_NA,
+    datediff(fs.encounter_datetime, fs.cd4_resulted_date) as Days_Since_Last_CD4,
     null as Entry_Point,
     case
         when et.name in ('ADULTINITIAL', 'PEDSINITIAL', 'YOUTHINITIAL') then 'Initial'
         when et.name in ('ADULTRETURN', 'PEDSRETURN', 'YOUTHRETURN') then 'Return'
         else 'Other'
-    end as Encounter_Type_Class,
+        end as Encounter_Type_Class,
     null as Education_Level,
     null as Occupation,
     null as Adherence_Counselling_Sessions,
-    mc.mrsDisplay as Clinic_Name,
+    l.name as Clinic_Name,
     replace(get_arv_names(fs.cur_arv_meds), '##', '+') as ART_regimen,
-    fs.visit_number as Visit_Number,
-    lag(dd.days_defaulted) over (order by dd.encounter_datetime) as Days_defaulted_prev_encounter,
-    if(dd.days_defaulted is null, 1, 0) as Days_defaulted_prev_encounter_NA,
+    dd.visit_number as Visit_Number,
+    days_defaulted_last_encounter as Days_defaulted_in_prev_enc,
+    if(days_defaulted_last_encounter is null, 1, 0) as Days_defaulted_in_prev_enc_NA,
     num_2wks_defaults_last_3visits,
     if(num_2wks_defaults_last_3visits is null, 1, 0) as num_2wks_defaults_last_3visits_NA,
-    0 as ever_defaulted_by_1m_in_last_1year,
-    1 as ever_defaulted_by_1m_in_last_1year_NA,
-    0 as ever_defaulted_by_1m_in_last_1year,
-    1 as ever_defaulted_by_1m_in_last_1year_NA,
-    timestampdiff(YEAR, p.birthdate, baseline.encounter_datetime) as Age_baseline,
-    p.gender as Gender_baseline,
-    null as Marital_status_baseline,
-    round(baseline.weight / ((baseline.height / 100) * (baseline.height / 100)), 2) as BMI_baseline,
-    null as Travel_time_baseline,
-    baseline.cur_who_stage as WHO_staging,
-    if(baseline.vl_resulted < 1000, 1, 0) as VL_suppression_baseline,
-    log10(baseline.vl_resulted + 1) as Viral_Load_log10_baseline,
-    baseline.hiv_status_disclosed as HIV_disclosure_baseline,
-    baseline.cur_arv_line as Regimen_Line,
-    coalesce(baseline.is_pregnant, 0) as Pregnancy_baseline,
-    case
-        when baseline.location_id in (55, 315, 19, 230, 26, 23, 319, 130, 313, 9, 78, 310, 20, 312, 12, 321, 8, 341)
-            then 'Urban'
-        when baseline.location_id in (65, 314, 64, 83, 316, 90, 135, 106, 86, 336, 91, 320, 74, 76, 79, 100, 311, 75)
-            then 'Rural'
-    end as Clinic_Location_baseline,
-    null as TB_Comorbidity_baseline,
-    baseline.cd4_resulted as CD4,
-    null as Education_Level_baseline,
-    null as Occupation_baseline,
-    null as Adherence_Counselling_Sessions_baseline,
-    mc_b.mrsDisplay as Clinic_Name_baseline,
-    replace(get_arv_names(fs.cur_arv_meds), '##', '+') as ART_regimen_baseline
-from ml_flat_hiv_summary_with_encounter_number as fs
-    left join ml_flat_hiv_summary_with_encounter_number baseline
+    coalesce(any_30d_defaults_1yr, 0) as ever_defaulted_by_1m_in_last_1year,
+    if(any_30d_defaults_1yr is null, 1, 0) as ever_defaulted_by_1m_in_last_1year_NA,
+    coalesce(any_30d_defaults_2yr, 0) as ever_defaulted_by_1m_in_last_2year,
+    if(any_30d_defaults_2yr is null, 1, 0) as ever_defaulted_by_1m_in_last_2year_NA,
+    Age_baseline,
+    Gender_baseline,
+    Marital_status_baseline,
+    BMI_baseline,
+    Travel_time_baseline,
+    WHO_staging_baseline,
+    VL_suppression_baseline,
+    Viral_Load_log10_baseline,
+    HIV_disclosure_baseline,
+    Regimen_Line_baseline,
+    Pregnancy_baseline,
+    Clinic_Location_baseline,
+    TB_Comorbidity_baseline,
+    CD4_baseline,
+    Education_Level_baseline,
+    Occupation_baseline,
+    Adherence_Counselling_Sessions_baseline,
+    Clinic_Name_baseline,
+    ART_regimen_baseline
+from flat_hiv_summary_v15b as fs
+    left join predictions.flat_ml_baseline_visit baseline
         on fs.person_id = baseline.person_id
-       and fs.encounter_id <> baseline.encounter_id
-    left join days_defaulted dd
+    left join predictions.flat_ml_days_defaulted dd
         on dd.encounter_id = fs.encounter_id
-       and dd.person_id = fs.person_id
+            and dd.person_id = fs.person_id
     join amrs.person p on p.person_id = fs.person_id
-    join amrs.encounter_type et on fs.encounter_type = et.encounter_type_id
-    left join mfl_codes mc on fs.location_id = mc.mflCode
-    left join mfl_codes mc_b on baseline.location_id = mc.mflCode
+    left join amrs.encounter_type et on fs.encounter_type = et.encounter_type_id
+    left join amrs.location l
+        on fs.location_id = l.location_id
+            and l.retired = 0
     left join num_2wk_defaults_last_3_visits 2wk_defaults
-         on 2wk_defaults.person_id = fs.person_id
-        and 2wk_defaults.encounter_id = fs.encounter_id
-where fs.location_id in (319)
-  and fs.rtc_date between date_sub(current_date(), interval 1 week) and current_date()
-  and (baseline.visit_number is null or baseline.visit_number = 1);
+        on 2wk_defaults.person_id = fs.person_id
+            and 2wk_defaults.encounter_id = fs.encounter_id
+    left join (
+        select person_id, if(days_defaulted >= 30, 1, 0) as any_30d_defaults_1yr
+        from defaults_by_days
+        where encounter_date between date_sub('2023-06-19', interval 1 year) and '2023-06-19'
+        group by person_id
+    ) as 1yr on 1yr.person_id = fs.person_id
+    left join (
+        select person_id, if(days_defaulted >= 30, 1, 0) as any_30d_defaults_2yr
+        from defaults_by_days
+        where encounter_date between date_sub('2023-06-19', interval 2 year) and '2023-06-19'
+        group by person_id
+    ) as 2yr on 2yr.person_id = fs.person_id
+where fs.location_id in (26,23,319,130,313,9,78,310,20,312,12,321,8,341,65,314,64,83,90,106,86,336,91,320,74,76,79,100,311,75)
+  -- test locations
+  and fs.location_id not in (195, 429, 430, 354)
+  -- filter encounters: 111 - LabResult, 99999 - lab encounter type
+  -- these encounters are post-visit lab result entries and should not appear in predicted data
+  and fs.encounter_type not in (111, 99999)
+  -- ET 116 - Transfer Encounter; 9998 - transfer expected to AMPATH clinic
+  and (fs.encounter_type != 116 or fs.transfer_in_location_id = 9998)
+  -- 9999 - transfered to non-AMPATH clinic, we discount these for the list of patient's that we care about trying to
+  -- proactively follow-up on since we do not anticipate these patient's returning to AMPATH. If they do, they will be
+  -- returned to normal status at whatever clinic they visit
+  and fs.transfer_in_location_id != 9999
+  and fs.rtc_date between ?startDate and ?endDate
+  -- allow this query to handle retrospective predictions
+  and encounter_datetime < fs.date_created
+  and (next_clinical_datetime_hiv >= fs.date_created
+    or next_clinical_datetime_hiv is null)
---
+++ SQL/stored_procedures/generate_flat_ml_baseline_visit_v1_0.sql
@@ -0,0 +1,92 @@
+drop procedure if exists generate_flat_ml_baseline_visit_v1_0;
+create definer = analytics procedure generate_flat_ml_baseline_visit_v1_0()
+begin
+    drop table if exists predictions.flat_ml_baseline_visit;
+    create table predictions.flat_ml_baseline_visit (
+        person_id                               int                                  not null
+            primary key,
+        Age_baseline                            int                                  null,
+        Gender_baseline                         varchar(50) charset utf8  default '' null,
+        Marital_status_baseline                 varchar(100)                         null,
+        BMI_baseline                            decimal(21, 2)                       null,
+        Travel_time_baseline                    varchar(100)                         null,
+        WHO_staging_baseline                    int                                  null,
+        VL_suppression_baseline                 int(1)                               null,
+        Viral_Load_log10_baseline               double                               null,
+        HIV_disclosure_baseline                 int                                  null,
+        Regimen_Line_baseline                   int                                  null,
+        Pregnancy_baseline                      int(4)                               null,
+        Clinic_Location_baseline                varchar(5)                           null,
+        TB_Comorbidity_baseline                 varchar(100)                         null,
+        CD4_baseline                            double                               null,
+        Education_Level_baseline                varchar(100)                         null,
+        Occupation_baseline                     varchar(100)                         null,
+        Adherence_Counselling_Sessions_baseline varchar(100)                         null,
+        Clinic_Name_baseline                    varchar(255) charset utf8 default '' null,
+        ART_regimen_baseline                    text charset utf8                    null
+    ) as
+    with ml_flat_hiv_summary as (
+        select
+            fs.*
+        from etl.flat_hiv_summary_v15b fs
+        where arv_start_date is not null
+          and is_clinical_encounter = 1
+          and encounter_datetime >= date('2016-01-01')
+          and not (
+                    rtc_date = prev_rtc_date
+                and encounter_type = prev_encounter_type_hiv
+                and encounter_datetime = prev_encounter_datetime_hiv
+            )
+    ),
+    visit_numbers as (
+        select
+            row_number() over (partition by person_id order by encounter_datetime) as visit_number,
+            person_id,
+            encounter_id
+        from ml_flat_hiv_summary
+    ),
+    -- Combine these into one table
+    ml_flat_hiv_summary_with_encounter_number as (
+        select
+            fs.*,
+            visit_number
+        from ml_flat_hiv_summary fs
+            join visit_numbers vn
+                on fs.person_id = vn.person_id
+                    and fs.encounter_id = vn.encounter_id
+    )
+    select
+        baseline.person_id,
+        timestampdiff(YEAR, p.birthdate, baseline.encounter_datetime) as Age_baseline,
+        p.gender as Gender_baseline,
+        null as Marital_status_baseline,
+        round(baseline.weight / ((baseline.height / 100) * (baseline.height / 100)), 2) as BMI_baseline,
+        null as Travel_time_baseline,
+        baseline.cur_who_stage as WHO_staging_baseline,
+        if(baseline.vl_resulted < 1000, 1, 0) as VL_suppression_baseline,
+        log10(baseline.vl_resulted + 1) as Viral_Load_log10_baseline,
+        baseline.hiv_status_disclosed as HIV_disclosure_baseline,
+        baseline.cur_arv_line as Regimen_Line_baseline,
+        coalesce(baseline.is_pregnant, 0) as Pregnancy_baseline,
+        case
+            when baseline.location_id in (55, 315, 19, 230, 26, 23, 319, 130, 313, 9, 78, 310, 20, 312, 12, 321, 8, 341)
+                then 'Urban'
+            when baseline.location_id in (65, 314, 64, 83, 316, 90, 135, 106, 86, 336, 91, 320, 74, 76, 79, 100, 311, 75)
+                then 'Rural'
+            end as Clinic_Location_baseline,
+        null as TB_Comorbidity_baseline,
+        baseline.cd4_resulted as CD4_baseline,
+        null as Education_Level_baseline,
+        null as Occupation_baseline,
+        null as Adherence_Counselling_Sessions_baseline,
+        l.name as Clinic_Name_baseline,
+        replace(etl.get_arv_names(baseline.cur_arv_meds), '##', '+') as ART_regimen_baseline
+    from
+        ml_flat_hiv_summary_with_encounter_number baseline
+            left join amrs.person p using (person_id)
+            left join amrs.location l
+                      on baseline.location_id = l.location_id
+                          and l.retired = 0
+    where baseline.visit_number = 1;
+end;
+
