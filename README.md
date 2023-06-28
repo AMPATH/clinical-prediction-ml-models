0b6357568cefe9453c49a59f9a681eae12b8167c
Ian <ian_bacher@brown.edu>
2023-06-27 22:46:34 -0400

    Some comments and docs

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The main objective of this project is to predict clinical outcomes using EHR Dat
 
 ## Docker Image
 
-To build the Docker image, first create a `config.yml` using the file in Docker Files/ called `config.example.yml` as a guide. Then build the Docker image:
+To build the Docker image, first create a `config.yml` using the file in the `docker-resources` folder called `config.example.yml` as a guide. Then build the Docker image:
 
 
 ```
@@ -15,5 +15,22 @@ docker build --tag kenya-prediction-<model_version>:<version> .
 And run it locally:
 
 ```
-docker run --rm -p 8000:8000 -v "$(pwd)/Docker Files/config.yml:/app/config.yml:ro" kenya-prediction-<model_version>:<version>
+docker run --rm -p 8000:8000 -v "$(pwd)/docker-resources/config.yml:/app/config.yml:ro" kenya-prediction-<model_version>:<version>
 ```
+
+## The API
+
+The Docker container produced here provides an API to run the models. The API endpoint is paired with a corresponding SQL script that describes how to extract the predictors for the model from the live database.
+
+## IIT
+
+### Cohorts
+
+For interruption in treatment, we treat patients as belonging to week-long cohorts, running from Monday to Sunday (for whatever reason, there are a small number of appointments scheduled on Saturdays and Sundays). Since appoinment date is not a factor in our prediction models, these cohorts are primarily logical groups.
+
+The idea of these cohorts is that the bulk of patients for a given week are gathered and scored on the Sunday the week before the week containing their appointment. For example, a patient with an appointment on June 26th, 2023 should have their results
+score on June 18th, 2023. Since patient appointments can be added on a rolling basis, we continue to update the week's appointments every day.
+
+Patient's whose appointment has already been seen are not re-scored, as the overall risk score should not change without an intervening appointment.
+
+This also means that we do not drop patients out of cohorts if their appointment date is moved to a different week. This should be discussed.
--- docker-resources/plumber.R
+++ docker-resources/plumber.R
@@ -1,12 +1,3 @@
-#
-# This is a Plumber API. You can run the API by clicking
-# the 'Run API' button above.
-#
-# Find out more about building APIs with Plumber here:
-#
-#    https://www.rplumber.io/
-#
-
 # libraries for the model
 library(tidyverse)
 library(h2o)
@@ -23,8 +14,9 @@ h2o.init()
 dbConfig <- config::get()
 
 ml_model <- h2o.loadModel(
-  "/app/model/1_StackedEnsemble_BestOfFamily_1_day_default_prediction"
+  "/app/model/y0_1days_IIT/StackedEnsemble_AllModels_1_AutoML_1_20230615_104136"
 )
+
 ml_sql <- read_file("/app/iit_prod_data_extract.sql")
 
 my_pool <- dbPool(
@@ -64,7 +56,7 @@ cors <- function(req, res) {
 }
 
 #* @apiTitle AMPATH Interruption in Treatment Prediction Model API
-#* @apiDescription This API provides a simple method to
+#* @apiDescription This API provides a simple method to run the model for a set of weeks
 
 #* @param startDate:string
 #* @param weeks:int
@@ -73,20 +65,19 @@ cors <- function(req, res) {
 #* @serializer json
 #* @post /predict
 function(
-  startDate,
+  startDate = Sys.Date(),
   weeks = "0"
 ) {
+  # handle parameters
+  # despite typings, the arguments are always strings
   start_date <- as.Date(startDate)
-  # fancy date gymnastics
+  num_weeks <- max(as.integer(weeks), 0)
+
+  # calculate the Monday for this week; this is our start date
   week_start <- week_start(start_date)
-
-  num_weeks <- as.integer(weeks)
-  if (num_weeks < 0) {
-
-  }
-
-  # same deal, but get end the week (always Sunday)
-  week_end <- week_end(clock::add_weeks(week_start, as.integer(weeks)))
+  # first, add the number of weeks requested, then calculate the Sunday
+  # of the resuling week; this is our end date
+  week_end <- week_end(clock::add_weeks(week_start, weeks))
 
   query <- DBI::sqlInterpolate(
     my_pool,
@@ -95,32 +86,52 @@ function(
     endDate = week_end
   )
 
+  # run the query, giving us a dataframe
   predictors <- DBI::dbGetQuery(my_pool, query)
 
-  h2o_predict_frame <- predictors %>% as.h2o()
+  # convert the dataframe to an h2o object, removing elements that are not predictors
+  h2o_predict_frame <- predictors %>%
+    select(-c(person_id, encounter_id, location_id, rtc_date))
+    as.h2o()
 
+  # run the predictions
+  # TODO Why does this seem to claim we're running in train / validate mode?
   result <- h2o.predict(ml_model, h2o_predict_frame)
 
+  # h2o persists both the frame *and* the result in it's cluster;
+  # however, we don't need them, so after this function returns, we delete them
   on.exit(h2o.rm(h2o_predict_frame))
   on.exit(h2o.rm(result))
 
+  # enrich the table of predictors with the results
   prediction_result <- predictors %>%
     bind_cols(as.data.frame(result)) %>%
+    # reduce data frame and rename the result
     select(person_id, encounter_id, location_id, rtc_date, predicted_prob_disengage = Disengaged) %>%
+    # calculate the patient's risk category
+    predict_risk()
     mutate(
       predicted_prob_disengage = predicted_prob_disengage,
       percentile = percent_rank(predicted_prob_disengage),
       predicted_risk = if_else(percentile > .9, "High Risk", if_else(percentile > .8, "Medium Risk", NA_character_)),
-      prediction_generated_date = Sys.Date(),
-      start_date = start_date,
-      end_date = week_end,
-      week = clock::date_format(clock::add_weeks(week_start(rtc_date), -1), format="%Y-W%U"),
       .keep = "unused"
     ) %>%
     select(-percentile)
+    %>%
+    # add per-row metadata about the run
+    mutate(
+      prediction_generated_date = Sys.time(),
+      model_version = "V5",
+      start_date = week_start,
+      end_date = week_end,
+      week = clock::date_format(clock::add_weeks(week_start(rtc_date), -1), format="%Y-W%U"),
+      .keep = "unused"
+    )
 
+  # add the rows from the prediction_result to the ml_weekly_predictions table
   # DBI::dbAppendTable(my_pool, SQL('predictions.ml_weekly_predictions'), prediction_result)
 
+  # return the result so the API returns *something*
   prediction_result
 }
 
