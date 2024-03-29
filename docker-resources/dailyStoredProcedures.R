ce5fe0e71652ae2248c99f710700178ae08fd950
Ian <ian.c.bacher@gmail.com>
2024-03-29 09:07:55 -0400

    Add cron job to run required stored procedures daily
    NB This means there can be a 24 hour delay on the observed RTC date

---
+++ docker-resources/dailyStoredProcedures.R
@@ -0,0 +1,29 @@
+# This doesn't need to be in R but is for convenience
+library(RMariaDB)
+library(DBI)
+library(pool)
+
+dbConfig <- config::get()
+
+my_pool <- dbPool(
+  drv = RMariaDB::MariaDB(),
+  host = dbConfig$host,
+  username = dbConfig$user,
+  password = dbConfig$password,
+  dbname = dbConfig$defaultDb
+)
+
+DBI::dbGetQuery(
+  my_pool,
+  "CALL predictions.generate_flat_ml_baseline_visit_v1_0();"
+)
+
+DBI::dbGetQuery(
+  my_pool,
+  "CALL predictions.generate_flat_ml_days_defaulted_v1_0();"
+)
+
+DBI::dbGetQuery(
+  my_pool,
+  "CALL predictions.add_observed_appointments_to_ml_predictions_v1_0();"
+)
