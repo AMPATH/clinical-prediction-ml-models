f90071a27797d5b8247fec7f4d6a28100daa0dff
Ian <ian.c.bacher@gmail.com>
2024-05-03 10:01:47 -0400

    Add encounter filter to observed return date.

--- SQL/stored_procedures/add_observed_appointment_to_ml_predictions_v1_0.sql
+++ SQL/stored_procedures/add_observed_appointment_to_ml_predictions_v1_0.sql
@@ -5,6 +5,7 @@ begin
         left join etl.flat_hiv_summary_v15b fs_next
         on mp.person_id = fs_next.person_id
             and mp.rtc_date = fs_next.prev_rtc_date
+            and fs_next.encounter_type not in (186, 99999)
             and fs_next.is_clinical_encounter = 1
             and (
                    fs_next.rtc_date is not null
