31e7e654cbf818bb05fd25205069f9f4d85a10e5
Ian <ian.c.bacher@gmail.com>
2024-11-04 08:25:55 -0500

    More MCH Clinics

--- SQL/iit_prod_data_extract.sql
+++ SQL/iit_prod_data_extract.sql
@@ -125,7 +125,9 @@ where
     -- Fountain HC added retro-actively
     147,
     -- Langas MCH added retro-actively
-    324
+    324,
+    -- Moi's Bridge, Moiben, Kapyemit Dispensary and Elgeyo Border Dispensary added retro-actively
+    325, 326, 399, 400
   )
   -- filter encounters: 111 - LabResult, 99999 - lab encounter type
   -- these encounters are post-visit lab result entries and should not appear in predicted data
